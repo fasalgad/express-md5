7a3084a6f2e2a346caadc4f6da70cce402f36e7c
Fabian Salgado <salgado.fabian@gmail.com>
2022-10-11 23:22:18 -0300

    modificando rutas

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.get('/', (req, res) => {
+    res.send('hello')
+})
+
+
 app.get('/:word', (req, res) => {
     res.send(md5(req.params.word))
 })
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`)
